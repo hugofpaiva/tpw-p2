1c43a6554b042fd92929b8f7f7c43104dee71ce7
Daniel Gomes <dagomes@ua.pt>
2021-01-07 19:55:36 +0000

    backend filters almost done

--- webproj/app/filters.py
+++ webproj/app/filters.py
@@ -1,21 +1,23 @@
 from django.db.models import Q
 from django_filters import rest_framework as filters
 from django_filters.fields import Lookup
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend, Filter
 
 from app.models import Product
-
+class ListFilter(filters.Filter):
+    def filter(self,qs,value):
+        if value not in (None,''):
+            strings = [str(v) for v in value.split(',')]
+            return qs.filter(**{'%s__%s'%(self.field_name, self.lookup_expr):strings})
+        return qs
 
 class ProductFilter(filters.FilterSet):
     name = filters.CharFilter(lookup_expr='icontains', exclude='')
     min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
     max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
-    category = filters.CharFilter(field_name='category__title', lookup_expr='in', exclude='')
-    developer = filters.CharFilter(method='look_anywhere',field_name='developer__name')
-
-    def look_anywhere(self, queryset, name, value):
-        print((name),value,queryset)
-        return queryset.filter(developer__in=value)
+    category = ListFilter(field_name='category__title', lookup_expr='in', exclude='')
+    developer = ListFilter(field_name='developer__name',lookup_expr='in')
+   
 
     class Meta:
         model = Product
