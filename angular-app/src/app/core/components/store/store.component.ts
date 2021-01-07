236e5184bf5d20e0df6e620bf8d684733fae3b85
bastos-01 <p.bastos112@gmail.com>
2021-01-07 16:20:47 +0000

    products showing

--- angular-app/src/app/core/components/store/store.component.ts
+++ angular-app/src/app/core/components/store/store.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Product} from '../../models/product';
 import {Developer} from '../../models/developer';
 import {Category} from '../../models/category';
+import {ProductService} from "../../services/product/product.service";
 
 @Component({
   selector: 'app-store',
@@ -13,10 +14,15 @@ export class StoreComponent implements OnInit {
   developers: Developer[] = [];
   categories: Category[] = [];
 
-  constructor() {
+  constructor(private productService: ProductService) {
   }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts().subscribe(products => this.products = products);
   }
 
 }
