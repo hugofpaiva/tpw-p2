f6cfc4dc151d20e455fe8cd44580c88b7b18d920
bastos-01 <p.bastos112@gmail.com>
2021-01-08 00:22:46 +0000

    initialpage

--- angular-app/src/app/core/components/initialpage/initialpage.component.ts
+++ angular-app/src/app/core/components/initialpage/initialpage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Product} from '../../models/product';
+import {ProductService} from '../../services/product/product.service';
 
 @Component({
   selector: 'app-initialpage',
@@ -9,10 +10,36 @@ import {Product} from '../../models/product';
 export class InitialpageComponent implements OnInit {
 
   products: Product[] = [];
+  productsBanner: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {
+
+  }
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getProductsBanner();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts().subscribe(products => this.products = products);
+    // console.log(this.products.length);
+  }
+
+  getProductsBanner(): void {
+    const max = Math.floor(Math.random() * (6 - 2 + 1)) + 2;
+
+    for ( const x of [].constructor(max)){
+      const index = Math.random() * (this.products.length - 1 - 0 + 1) + 0;
+      console.log(this.products.length);
+      const prod: Product = this.products[index];
+      if (!this.productsBanner.includes(prod)){
+        console.log('ok');
+        this.productsBanner.push(prod);
+        console.log(prod);
+      }
+
+    }
   }
 
 }
