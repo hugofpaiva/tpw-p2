b0712b86da5e25fd2f2eece4c81a27e65eee5c64
hugofpaiva <hugofpaiva@ua.pt>
2021-01-10 02:31:21 +0000

    Multiple fixes

--- angular-app/src/assets/js/main.js
+++ angular-app/src/assets/js/main.js
@@ -1,11 +1,3 @@
-/*  ---------------------------------------------------
-    Template Name: Male Fashion
-    Description: Male Fashion - ecommerce teplate
-    Author: Colorib
-    Author URI: https://www.colorib.com/
-    Version: 1.0
-    Created: Colorib
----------------------------------------------------------  */
 
 'use strict';
 
@@ -28,27 +20,7 @@
         allowParentLinks: true
     });
 
-    /*------------------
-        Accordin Active
-    --------------------*/
-    $('.collapse').on('shown.bs.collapse', function () {
-        $(this).prev().addClass('active');
-    });
 
-    $('.collapse').on('hidden.bs.collapse', function () {
-        $(this).prev().removeClass('active');
-    });
-
-    //Canvas Menu
-    $(".canvas__open").on('click', function () {
-        $(".offcanvas-menu-wrapper").addClass("active");
-        $(".offcanvas-menu-overlay").addClass("active");
-    });
-
-    $(".offcanvas-menu-overlay").on('click', function () {
-        $(".offcanvas-menu-wrapper").removeClass("active");
-        $(".offcanvas-menu-overlay").removeClass("active");
-    });
 
     /*-----------------------
         Hero Slider
